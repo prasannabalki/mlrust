b87595097a279d79d9223fc4646d2c614ecb5b7e
Prasanna <balkiprasanna1984@gmail.com>
2019-12-29 18:28:00 +0530

    Sample rust code

---
+++ sample/hello_world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+C:\Users\Welcome\coderepos\mlrust\sample\hello_world\target\debug\deps\hello_world.exe: src\main.rs
+
+C:\Users\Welcome\coderepos\mlrust\sample\hello_world\target\debug\deps\hello_world.d: src\main.rs
+
+src\main.rs:
